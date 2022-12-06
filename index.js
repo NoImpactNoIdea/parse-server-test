29070740ec6b7ec0e16a3517324a25d8ee2d942d
Charlie Arcodia <noimpactapplications@gmail.com>
2022-12-06 10:35:44 -0700

    test

--- index.js
+++ index.js
@@ -14,42 +14,77 @@ if (!databaseUri) {
 }
 console.log(`database uri is: ${databaseUri}`);
 
-const config = {
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+var api = new ParseServer({
+  databaseURI: databaseUri || "mongodb://localhost:27017/dev",
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + "/cloud/main.js",
   appId: process.env.APP_ID,
-  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL, // Don't forget to change to https if needed
+  masterKey: process.env.MASTER_KEY,
+  serverURL: process.env.SERVER_URL,
   liveQuery: {
-    classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
+    classNames: ["Track"],
   },
-};
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
+  auth: {
+    apple: {
+      client_id: process.env.IOS_BUNDLE_ID,
+    },
+  },
+  verifyUserEmails: false,
+  appName: "suiteTest",
+  publicServerURL: process.env.SERVER_URL,
+  emailVerifyTokenValidityDuration: 2 * 60 * 60,
 
-const app = express();
+});
+
+var app = express();
 
 // Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 
 // Serve the Parse API on the /parse URL prefix
-const mountPath = process.env.PARSE_MOUNT || '/parse';
-if (!test) {
-  const api = new ParseServer(config);
-  app.use(mountPath, api);
-}
+var mountPath = process.env.PARSE_MOUNT || "/parse";
+app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function (req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+// PS plays nicely with the rest of your web routes
+app.get("/", function (req, res) {
+  res.status(200).send("This server is ready for Suite16-test");
 });
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
+// const config = {
+//   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+//   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+//   appId: process.env.APP_ID,
+//   masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+//   serverURL: process.env.SERVER_URL, // Don't forget to change to https if needed
+//   liveQuery: {
+//     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
+//   },
+// };
+// // Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// // If you wish you require them, you can set them as options in the initialization above:
+// // javascriptKey, restAPIKey, dotNetKey, clientKey
+
+// const app = express();
+
+// // Serve static assets from the /public folder
+// app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// // Serve the Parse API on the /parse URL prefix
+// const mountPath = process.env.PARSE_MOUNT || '/parse';
+// if (!test) {
+//   const api = new ParseServer(config);
+//   app.use(mountPath, api);
+// }
+
+// // Parse Server plays nicely with the rest of your web routes
+// app.get('/', function (req, res) {
+//   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+// });
+
+// // There will be a test page available on the /test path of your server url
+// // Remove this before launching your app
+// app.get('/test', function (req, res) {
+//   res.sendFile(path.join(__dirname, '/public/test.html'));
+// });
 
 const port = process.env.PORT || 1337;
 if (!test) {
