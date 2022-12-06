47dfd267b2d051131de0c651b514004c466310e2
Charlie Arcodia <noimpactapplications@gmail.com>
2022-12-05 18:55:28 -0700

    more

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
+console.log(`database uri is: ${databaseUri}`);
+
 const config = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  appId: '47830504903837484',
+  masterKey: '430984567324984', //Add your master key here. Keep it secret!
+  serverURL: 'https://parse-tester-example.herokuapp.com/', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
